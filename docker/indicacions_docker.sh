91b7674aa4fd6be2770e8329e33bf03dff7b6932
jrdvti <joan.romeu@davinciti.com>
2025-10-10 12:30:49 +0200

    Create indicacions_docker.sh

---
+++ docker/indicacions_docker.sh
@@ -0,0 +1,11 @@
+# Obrim terminal
+# Obrim Docker Desktop
+# Anem a la carpeta on s'ha guardat el docker-compose.yml i executem
+
+docker-compose up -d
+
+# Mirem que al Docker Deskotp estigui el contenidor
+# Esperem una estona
+# Fem click on surt
+
+http://localhost:5601
